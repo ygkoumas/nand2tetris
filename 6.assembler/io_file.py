bbb247f64744df848ce05a3b05d4f8f8e1b4237e
Yannis Gkoumas <ygkoumas@gmail.com>
2016-10-17 23:53:41 +0100

    Add array_to_file method

--- 6.assembler/io_file.py
+++ 6.assembler/io_file.py
@@ -6,3 +6,9 @@ class IOFile:
 	def file_to_array(self):
 		fo = open(self.shared_data.input_file_name, 'r')
 		self.shared_data.io_file = fo.readlines()
+
+	def array_to_file(self):
+		file_name = self.shared_data.io_file.replase('asm', 'hack')
+		fo = open(file_name, 'w')
+		fo.write('\n'.join(self.shared_data.code))
+		fo.close()

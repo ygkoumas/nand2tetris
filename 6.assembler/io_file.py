e129dcc70be295c5c7f738fc03ecff1d53ed417e
Yannis Gkoumas <ygkoumas@gmail.com>
2016-10-18 23:38:18 +0100

    Fix a typos in array_to_file method

--- 6.assembler/io_file.py
+++ 6.assembler/io_file.py
@@ -8,7 +8,7 @@ class IOFile:
 		self.shared_data.io_file = fo.readlines()
 
 	def array_to_file(self):
-		file_name = self.shared_data.io_file.replase('asm', 'hack')
+		file_name = self.shared_data.io_file.replace('asm', 'hack')
 		fo = open(file_name, 'w')
 		fo.write('\n'.join(self.shared_data.code))
 		fo.close()

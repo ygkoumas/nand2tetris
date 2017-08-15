ebbaea44a0e9d1f181ea80d8b4a20fa7972a8177
Yannis Gkoumas <ygkoumas@gmail.com>
2017-08-15 22:02:50 +0100

    Simplify file input filter

--- 6.assembler/io_file.py
+++ 6.assembler/io_file.py
@@ -8,7 +8,7 @@ class IOFile:
 	def file_to_array(self):
 		fo = open(self.shared_data.input_file_name, 'r')
 		file_list = map(lambda line: re.sub('[\s\t\n]|\/\/.*', '', line), fo.readlines())
-		self.shared_data.io_file = filter(lambda line: True if line != '' else False, file_list)
+		self.shared_data.io_file = filter(lambda line: line != '', file_list)
 
 	def array_to_file(self):
 		file_name = self.shared_data.input_file_name.replace('asm', 'hack')

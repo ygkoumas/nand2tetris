f489af3effff4572d2d42a5867470048f54f86ed
Yannis Gkoumas <ygkoumas@gmail.com>
2017-08-15 22:17:51 +0100

    Use with-open ensuring files are closed

--- 6.assembler/io_file.py
+++ 6.assembler/io_file.py
@@ -6,13 +6,12 @@ class IOFile:
 		self.shared_data = shared_data
 
 	def file_to_array(self):
-		fo = open(self.shared_data.input_file_name, 'r')
-		file_list = map(lambda line: re.sub('[\s\t\n]|\/\/.*', '', line), fo.readlines())
+		with open(self.shared_data.input_file_name, 'r') as fo:
+			raw_file_list = fo.readlines()
+		file_list = map(lambda line: re.sub('[\s\t\n]|\/\/.*', '', line), raw_file_list)
 		self.shared_data.io_file = filter(lambda line: line != '', file_list)
 
 	def array_to_file(self):
 		file_name = self.shared_data.input_file_name.replace('asm', 'hack')
-		fo = open(file_name, 'w')
-		fo.write('\n'.join(self.shared_data.unparser))
-		fo.close()
-
+		with open(file_name, 'w') as fo:
+			fo.write('\n'.join(self.shared_data.unparser))

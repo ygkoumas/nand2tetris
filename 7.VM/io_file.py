89673155d21679e1eb647a18bf6686af0b75d9aa
Yannis Gkoumas <ygkoumas@gmail.com>
2017-08-20 12:17:36 +0100

    Use with-open ensuring files are closed on vm-translator

--- 7.VM/io_file.py
+++ 7.VM/io_file.py
@@ -1,20 +1,18 @@
 import re
 
 class IOFile:
-	'''converts input file.asm to array and processed array to output file.hack'''
+	'''converts input file.vm to array and processed array to output file.asm'''
 	def __init__(self, shared_data):
 		self.shared_data = shared_data
 
 	def file_to_array(self):
-		fo = open(self.shared_data.input_file_name, 'r')
-		file_list = map(lambda line: re.sub('[\t\n]|\/\/.*', ' ', line), fo.readlines())
+		with open(self.shared_data.input_file_name, 'r') as fo:
+			raw_file_list = fo.readlines()
+		file_list = map(lambda line: re.sub('[\t\n]|\/\/.*', ' ', line), raw_file_list)
 		file_list = map(lambda line: re.sub('\s\s', ' ', line), file_list)
-		file_list = filter(lambda line: True if line != ' ' and line != '' else False, file_list)
-		self.shared_data.io_file = file_list
+		self.shared_data.io_file = filter(lambda line: line != '' and line !=' ', file_list)
 
 	def array_to_file(self):
 		file_name = self.shared_data.input_file_name.replace('vm', 'asm')
-		fo = open(file_name, 'w')
-		fo.write('\n'.join(self.shared_data.code_writer))
-		fo.close()
-
+		with open(file_name, 'w') as fo:
+			fo.write('\n'.join(self.shared_data.code_writer))
